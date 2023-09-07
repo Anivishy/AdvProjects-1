6c9e1f5645f284cf3e93c1a5d903290c44ff6080
AniVishy <any.vishy@outlook.com>
2023-09-07 13:08:53 -0700

    Commit

--- hangman.py
+++ hangman.py
@@ -16,3 +16,14 @@ pygame.display.set_caption('Hangman')
 word_file = open("words.txt", "r")
 
 wordlist = word_file.readlines()
+
+def pick_word():
+    return wordlist[random.randint(0, len(wordlist))]
+
+running = True
+while running:
+    for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.QUIT:
+            running = False
+            
