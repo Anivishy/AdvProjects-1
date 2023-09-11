0e9b0318a11a0938b740af73a4011ddfc59eb4b4
AniVishy <any.vishy@outlook.com>
2023-09-11 13:24:25 -0700

    Started display font work

--- hangman.py
+++ hangman.py
@@ -2,27 +2,30 @@ import pygame, sys, random
 from pygame.locals import *
 pygame.init()
 
+#window init
 background_color = (0,0,0)
-
 FPS =  60
 fps_clock = pygame.time.Clock()
-
 WINDOW_WIDTH = 400
-WINDOW_HEIGHT = 300
- 
+WINDOW_HEIGHT = 300 
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Hangman')
 
-word_file = open("words.txt", "r")
+#font init
+font = pygame.font.SysFont('arial', 50)
 
+#wordlist init
+word_file = open("words.txt", "r")
 wordlist = word_file.readlines()
 
+#wordlist functions
 def pick_word():
     return wordlist[random.randint(0, len(wordlist) - 1)]
 
 def check_word(letter :str, word :str):    
     if letter in word:
         return True
+   
 
 running = True
 target_word = pick_word()
@@ -30,7 +33,8 @@ print(target_word)
 while running:    
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
-            print(event.unicode)           
+            letter = event.unicode
+                       
         if event.type == pygame.QUIT:
             running = False
             
