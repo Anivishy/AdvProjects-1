d624a5aad72fef647eeb2acd1a3aee59999f05ed
AniVishy <any.vishy@outlook.com>
2023-09-12 12:34:58 -0700

    Test

--- hangman.py
+++ hangman.py
@@ -2,12 +2,15 @@ import pygame, sys, random
 from pygame.locals import *
 pygame.init()
 
+#colors
+GREY = (255, 255, 255)
+
 #window init
 background_color = (0,0,0)
 FPS =  60
 fps_clock = pygame.time.Clock()
-WINDOW_WIDTH = 400
-WINDOW_HEIGHT = 300 
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 500 
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Hangman')
 
@@ -25,11 +28,17 @@ def pick_word():
 def check_word(letter :str, word :str):    
     if letter in word:
         return True
+    
+#hangman drawing
+def hangman_draw_init():
+    print("Test")
+    pygame.draw.line(WINDOW, GREY, (0,0), (0,0), 20)
    
 
 running = True
 target_word = pick_word()
 print(target_word)
+hangman_draw_init()
 while running:    
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
