25cfacc85c78f1e695cd57b574ec3e9b66c35dcd
AniVishy <any.vishy@outlook.com>
2023-09-12 13:44:14 -0700

    Hangman graphics

--- hangman.py
+++ hangman.py
@@ -4,15 +4,16 @@ from pygame.locals import *
 pygame.init()
 
 #colors
-GREY = (255, 255, 255)
+GREY = (128, 128, 128)
+WHITE = (225, 225, 225)
+BLACK = (0, 0, 0)
 
 #window init
-background_color = (0,0,0)
 FPS =  60
 fps_clock = pygame.time.Clock()
 WINDOW_WIDTH = 500
 WINDOW_HEIGHT = 500 
-WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Hangman')
 
 #font init
@@ -33,18 +34,35 @@ def check_word(letter :str, word :str):
     
 #hangman drawing
 def hangman_draw_init():
-    print("Test")
-    pygame.draw.line(WINDOW, GREY, (0,0), (0,0), 20)
+    #pygame.draw.line(surface, GREY, (25,400), (200,400), 10)
+    pygame.draw.line(surface, GREY, (25,400), (200,400), 10) #platform
+    pygame.draw.line(surface, GREY, (50,400), (50,100), 10) #post
+    pygame.draw.line(surface, GREY, (25,100), (150,100), 10) #top
+    pygame.draw.line(surface, GREY, (125,100), (125,150), 10) #rope
+    pygame.draw.circle(surface, GREY, (125, 180), 30, 3) #head
+    pygame.draw.line(surface, GREY, (125, 210), (125, 300), 3) #body
+    pygame.draw.line(surface, GREY, (125, 210), (75, 250), 3) #left hand
+    pygame.draw.line(surface, GREY, (125, 210), (175, 250), 3) #right hand
+    pygame.draw.line(surface, GREY, (125, 210), (75, 250), 3) #left leg
+    pygame.draw.line(surface, GREY, (125, 210), (175, 250), 3) #right right leg
+    
+    
    
 
 running = True
 target_word = pick_word()
 print(target_word)
+surface.fill(BLACK)
 hangman_draw_init()
+pygame.display.update() 
+
 while running:    
+    
+    
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             letter = event.unicode
+            print(event.unicode)
                        
         if event.type == pygame.QUIT:
             running = False
